70a3f1d08837fb7ca25bcedbfc60ce6151794491
sudhanshu <sudhanshuskr2000@gmail.com>
2024-03-06 15:00:59 -0800

    added existing files

---
+++ camera_module_ws (copy)/src/yolov4/yolov4/oakd_pro_camera_run.py
@@ -0,0 +1,189 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+from pathlib import Path
+import sys
+import cv2
+import depthai as dai
+import numpy as np
+import time
+import argparse
+import json
+import blobconverter
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'camera_detection', 10)
+        timer_period = 5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        
+        #!/usr/bin/env python3
+        """
+        The code is edited from docs (https://docs.luxonis.com/projects/api/en/latest/samples/Yolo/tiny_yolo/)
+        We add parsing from JSON files that contain configuration
+        """
+
+        # parse arguments
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-m", "--model", help="Provide model name or model path for inference",
+                            default='/home/sudhanshu/Lab_code/new_package_ws/src/yolo_test_1/model/yolov4_tiny_coco_416x416_v2.blob', type=str)
+        parser.add_argument("-c", "--config", help="Provide config path for inference",
+                            default='/home/sudhanshu/Lab_code/new_package_ws/src/yolo_test_1/json/yolov4-tiny.json', type=str)
+        args = parser.parse_args()
+
+        # parse config
+        configPath = Path(args.config)
+        if not configPath.exists():
+            raise ValueError("Path {} does not exist!".format(configPath))
+
+        with configPath.open() as f:
+            config = json.load(f)
+        nnConfig = config.get("nn_config", {})
+
+        # parse input shape
+        if "input_size" in nnConfig:
+            W, H = tuple(map(int, nnConfig.get("input_size").split('x')))
+
+        # extract metadata
+        metadata = nnConfig.get("NN_specific_metadata", {})
+        classes = metadata.get("classes", {})
+        coordinates = metadata.get("coordinates", {})
+        anchors = metadata.get("anchors", {})
+        anchorMasks = metadata.get("anchor_masks", {})
+        iouThreshold = metadata.get("iou_threshold", {})
+        confidenceThreshold = metadata.get("confidence_threshold", {})
+
+        print(metadata)
+
+        # parse labels
+        nnMappings = config.get("mappings", {})
+        labels = nnMappings.get("labels", {})
+
+        # get model path
+        nnPath = args.model
+        if not Path(nnPath).exists():
+            print("No blob found at {}. Looking into DepthAI model zoo.".format(nnPath))
+            nnPath = str(blobconverter.from_zoo(args.model, shaves = 6, zoo_type = "depthai", use_cache=True))
+        # sync outputs
+        syncNN = True
+
+        # Create pipeline
+        pipeline = dai.Pipeline()
+
+        # Define sources and outputs
+        camRgb = pipeline.create(dai.node.ColorCamera)
+        detectionNetwork = pipeline.create(dai.node.YoloDetectionNetwork)
+        xoutRgb = pipeline.create(dai.node.XLinkOut)
+        nnOut = pipeline.create(dai.node.XLinkOut)
+
+        xoutRgb.setStreamName("rgb")
+        nnOut.setStreamName("nn")
+
+        # Properties
+        camRgb.setPreviewSize(W, H)
+
+        camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        camRgb.setInterleaved(False)
+        camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+        camRgb.setFps(40)
+
+        # Network specific settings
+        detectionNetwork.setConfidenceThreshold(confidenceThreshold)
+        detectionNetwork.setNumClasses(classes)
+        detectionNetwork.setCoordinateSize(coordinates)
+        detectionNetwork.setAnchors(anchors)
+        detectionNetwork.setAnchorMasks(anchorMasks)
+        detectionNetwork.setIouThreshold(iouThreshold)
+        detectionNetwork.setBlobPath(nnPath)
+        detectionNetwork.setNumInferenceThreads(2)
+        detectionNetwork.input.setBlocking(False)
+
+        # Linking
+        camRgb.preview.link(detectionNetwork.input)
+        detectionNetwork.passthrough.link(xoutRgb.input)
+        detectionNetwork.out.link(nnOut.input)
+
+        # Connect to device and start pipeline
+        with dai.Device(pipeline) as device:
+
+            # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+            qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+            qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+
+            frame = None
+            detections = []
+            startTime = time.monotonic()
+            counter = 0
+            color2 = (255, 255, 255)
+
+            # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
+            def frameNorm(frame, bbox):
+                normVals = np.full(len(bbox), frame.shape[0])
+                normVals[::2] = frame.shape[1]
+                return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+            def displayFrame(name, frame, detections,count,sf):
+                color = (0, 255, 0)
+                for detection in detections:
+                    bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+                    cv2.putText(frame, labels[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+                    cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+                    cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
+                    if count%sf ==0 : 
+                        #print("Detected label : \'",labels[detection.label],"\' || Centered at : ",(0.5*(bbox[0]+ bbox[2]), 0.5*(bbox[1]+ bbox[3])))
+                        msg = String()
+                        msg.data = "Detected label : \'"+str(labels[detection.label])+"\' || Centered at : "+str((0.5*(bbox[0]+ bbox[2])+ 0.5*(bbox[1]+ bbox[3])))
+                        self.publisher_.publish(msg)
+                        self.get_logger().info('Publishing: "%s"' % msg.data)
+                        self.i += 1
+                # Show the frame
+                cv2.imshow(name, frame)
+
+            count = 1
+            sampling_freq = 50
+            while True:
+                inRgb = qRgb.get()
+                inDet = qDet.get()
+
+                if inRgb is not None:
+                    frame = inRgb.getCvFrame()
+                    cv2.putText(frame, "NN fps: {:.2f}".format(counter / (time.monotonic() - startTime)),
+                                (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color2)
+
+                if inDet is not None:
+                    detections = inDet.detections
+                    counter += 1
+
+                if frame is not None:
+                    displayFrame("rgb", frame, detections,count,sampling_freq)
+                    count += 1
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ camera_module_ws/src/build/yolov4/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/sudhanshu/Lab_code/camera_module_ws/src/install/yolov4'
